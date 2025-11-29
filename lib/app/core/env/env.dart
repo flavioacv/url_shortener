fa60c276904dc3ce84c7fbcc1264dd0bce8c1c34
Flavio Augusto <flavioacv.soft@gmail.com>
2025-11-28 22:09:44 -0400

    refactor: remove flutter_dotenv package and hardcode base URL

--- lib/app/core/env/env.dart
+++ lib/app/core/env/env.dart
@@ -1,23 +1,10 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 class EnvConfig {
   static final EnvConfig _instance = EnvConfig._internal();
-
   factory EnvConfig() {
     return _instance;
   }
-
   EnvConfig._internal();
 
-  static Future<void> load() async {
-    await dotenv.load();
-  }
-
-  String get baseUrl => dotenv.env['BASE_URL'] ?? '';
-  String get clientIdUrlGov => dotenv.env['CLIENT_ID_URL_GOV'] ?? '';
-  String get redirectUrlGov => dotenv.env['REDIRECT_URL_GOV'] ?? '';
-  String get schemeGov => dotenv.env['SCHEME'] ?? '';
-  String get hostGov => dotenv.env['HOST'] ?? '';
-  String get pathGov => dotenv.env['PATH'] ?? '';
-  String get enviroment => dotenv.env['ENVIRONMENT'] ?? '';
+  
+  String get baseUrl => 'https://url-shortener-server.onrender.com';
 }
