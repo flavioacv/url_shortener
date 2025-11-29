19d9f310bf76fa0a82d4d08515075bf64acb1b49
Flavio Augusto <flavioacv.soft@gmail.com>
2025-11-28 22:22:15 -0400

    feat: Redefine and categorize the app color palette with new purple, neutral, success, and error shades.

--- lib/app/core/themes/colors/app_colors.dart
+++ lib/app/core/themes/colors/app_colors.dart
@@ -1,93 +1,163 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
-/// A class that defines the color scheme for the Eleven Dash App.
-///
-/// This class extends the `ThemeExtension` class and provides a set of colors
-/// that can be used to style the app's UI elements. The colors are defined as
-/// instance variables and can be accessed directly. The class also provides
-/// methods to create copies of the theme with modified colors and to interpolate
-/// between two themes.
 class AppColors extends ThemeExtension<AppColors> {
-  final Color green;
+  // Purple Colors (Brand)
+  final Color purple600;
+  final Color purple700;
+  final Color purple800;
+  final Color purple50;
+  final Color purple300;
+  final Color purple100;
 
-  final Color yellow;
-
+  // Neutral Colors (Grays)
   final Color white;
+  final Color gray50;
+  final Color gray100;
+  final Color gray200;
+  final Color gray300;
+  final Color gray400;
+  final Color gray500;
+  final Color gray600;
+  final Color gray700;
+  final Color gray900;
 
-  final Color gray;
+  // Success Colors (Green)
+  final Color green500;
+  final Color green50;
+  final Color green200;
+  final Color green800;
 
-  final Color lightGray;
-
-  final Color black;
-
-  final Color blue;
-
-  final Color red;
-
-  final Color lightRed;
-
-  final Color silver;
-
-  final Color bronze;
+  // Error Colors (Red)
+  final Color red500;
+  final Color red50;
+  final Color red200;
+  final Color red800;
 
   const AppColors({
-    required this.green,
-    required this.yellow,
+    // Purple
+    required this.purple600,
+    required this.purple700,
+    required this.purple800,
+    required this.purple50,
+    required this.purple300,
+    required this.purple100,
+    // Neutral
     required this.white,
-    required this.gray,
-    required this.lightGray,
-    required this.black,
-    required this.blue,
-    required this.red,
-    required this.lightRed,
-    required this.silver,
-    required this.bronze,
+    required this.gray50,
+    required this.gray100,
+    required this.gray200,
+    required this.gray300,
+    required this.gray400,
+    required this.gray500,
+    required this.gray600,
+    required this.gray700,
+    required this.gray900,
+    // Success
+    required this.green500,
+    required this.green50,
+    required this.green200,
+    required this.green800,
+    // Error
+    required this.red500,
+    required this.red50,
+    required this.red200,
+    required this.red800,
   });
 
   @override
   AppColors copyWith({
-    Color? green,
-    Color? blue,
-    Color? yellow,
+    // Purple
+    Color? purple600,
+    Color? purple700,
+    Color? purple800,
+    Color? purple50,
+    Color? purple300,
+    Color? purple100,
+    // Neutral
     Color? white,
-    Color? gray,
-    Color? lightGray,
-    Color? black,
-    Color? red,
-    Color? lightRed,
-    Color? silver,
-    Color? bronze,
+    Color? gray50,
+    Color? gray100,
+    Color? gray200,
+    Color? gray300,
+    Color? gray400,
+    Color? gray500,
+    Color? gray600,
+    Color? gray700,
+    Color? gray900,
+    // Success
+    Color? green500,
+    Color? green50,
+    Color? green200,
+    Color? green800,
+    // Error
+    Color? red500,
+    Color? red50,
+    Color? red200,
+    Color? red800,
   }) {
     return AppColors(
-      green: green ?? this.green,
-      yellow: yellow ?? this.yellow,
+      // Purple
+      purple600: purple600 ?? this.purple600,
+      purple700: purple700 ?? this.purple700,
+      purple800: purple800 ?? this.purple800,
+      purple50: purple50 ?? this.purple50,
+      purple300: purple300 ?? this.purple300,
+      purple100: purple100 ?? this.purple100,
+      // Neutral
       white: white ?? this.white,
-      gray: gray ?? this.gray,
-      lightGray: lightGray ?? this.lightGray,
-      black: black ?? this.black,
-      blue: blue ?? this.blue,
-      red: red ?? this.red,
-      lightRed: lightRed ?? this.lightRed,
-      silver: silver ?? this.silver,
-      bronze: bronze ?? this.bronze,
+      gray50: gray50 ?? this.gray50,
+      gray100: gray100 ?? this.gray100,
+      gray200: gray200 ?? this.gray200,
+      gray300: gray300 ?? this.gray300,
+      gray400: gray400 ?? this.gray400,
+      gray500: gray500 ?? this.gray500,
+      gray600: gray600 ?? this.gray600,
+      gray700: gray700 ?? this.gray700,
+      gray900: gray900 ?? this.gray900,
+      // Success
+      green500: green500 ?? this.green500,
+      green50: green50 ?? this.green50,
+      green200: green200 ?? this.green200,
+      green800: green800 ?? this.green800,
+      // Error
+      red500: red500 ?? this.red500,
+      red50: red50 ?? this.red50,
+      red200: red200 ?? this.red200,
+      red800: red800 ?? this.red800,
     );
   }
 
   @override
   AppColors lerp(covariant AppColors? other, double t) {
     return AppColors(
-      green: Color.lerp(green, other?.green, t) ?? green,
-      yellow: Color.lerp(yellow, other?.yellow, t) ?? yellow,
+      // Purple
+      purple600: Color.lerp(purple600, other?.purple600, t) ?? purple600,
+      purple700: Color.lerp(purple700, other?.purple700, t) ?? purple700,
+      purple800: Color.lerp(purple800, other?.purple800, t) ?? purple800,
+      purple50: Color.lerp(purple50, other?.purple50, t) ?? purple50,
+      purple300: Color.lerp(purple300, other?.purple300, t) ?? purple300,
+      purple100: Color.lerp(purple100, other?.purple100, t) ?? purple100,
+      // Neutral
       white: Color.lerp(white, other?.white, t) ?? white,
-      gray: Color.lerp(gray, other?.gray, t) ?? gray,
-      lightGray: Color.lerp(lightGray, other?.lightGray, t) ?? lightGray,
-      black: Color.lerp(black, other?.black, t) ?? black,
-      blue: Color.lerp(blue, other?.blue, t) ?? blue,
-      red: Color.lerp(red, other?.red, t) ?? red,
-      lightRed: Color.lerp(lightRed, other?.lightRed, t) ?? lightRed,
-      silver: Color.lerp(silver, other?.silver, t) ?? silver,
-      bronze: Color.lerp(bronze, other?.bronze, t) ?? bronze,
+      gray50: Color.lerp(gray50, other?.gray50, t) ?? gray50,
+      gray100: Color.lerp(gray100, other?.gray100, t) ?? gray100,
+      gray200: Color.lerp(gray200, other?.gray200, t) ?? gray200,
+      gray300: Color.lerp(gray300, other?.gray300, t) ?? gray300,
+      gray400: Color.lerp(gray400, other?.gray400, t) ?? gray400,
+      gray500: Color.lerp(gray500, other?.gray500, t) ?? gray500,
+      gray600: Color.lerp(gray600, other?.gray600, t) ?? gray600,
+      gray700: Color.lerp(gray700, other?.gray700, t) ?? gray700,
+      gray900: Color.lerp(gray900, other?.gray900, t) ?? gray900,
+      // Success
+      green500: Color.lerp(green500, other?.green500, t) ?? green500,
+      green50: Color.lerp(green50, other?.green50, t) ?? green50,
+      green200: Color.lerp(green200, other?.green200, t) ?? green200,
+      green800: Color.lerp(green800, other?.green800, t) ?? green800,
+      // Error
+      red500: Color.lerp(red500, other?.red500, t) ?? red500,
+      red50: Color.lerp(red50, other?.red50, t) ?? red50,
+      red200: Color.lerp(red200, other?.red200, t) ?? red200,
+      red800: Color.lerp(red800, other?.red800, t) ?? red800,
     );
   }
 }
--- lib/app/core/themes/theme_data.dart
+++ lib/app/core/themes/theme_data.dart
@@ -5,23 +5,42 @@ import 'colors/app_colors.dart';
 final themeData = ThemeData(
   brightness: Brightness.light,
   colorScheme: ColorScheme.fromSeed(
-    seedColor: const Color.fromARGB(255, 25, 78, 69),
+    seedColor: const Color(0xFF9333EA), // purple-600
+    primary: const Color(0xFF9333EA), // purple-600
+    secondary: const Color(0xFF7E22CE), // purple-700
   ),
   useMaterial3: true,
-  scaffoldBackgroundColor: const Color(0xFFF2F2F2),
+  scaffoldBackgroundColor: const Color(0xFFF9FAFB), // gray-50
   extensions: const [
     AppColors(
-      green: Color(0xFF00823C),
-      yellow: Color(0xFFFABF18),
-      white: Color(0xFFFFFFFF),
-      gray: Color(0xFF888888),
-      lightGray: Color(0xFFF5F5F5),
-      black: Color(0xFF1C1C1C),
-      blue: Color(0xFF2864AE),
-      red: Color(0xFFDA1E28),
-      lightRed: Color(0xFFFA3117),
-      silver: Color(0xFFC0C0C0),
-      bronze: Color(0xFFCD7F32),
+      // Purple Colors (Brand)
+      purple600: Color(0xFF9333EA), // Primary brand color
+      purple700: Color(0xFF7E22CE), // Hover state
+      purple800: Color(0xFF6B21A8), // Active state
+      purple50: Color(0xFFF3E8FF), // Light background
+      purple300: Color(0xFFD8B4FE), // Border for new items
+      purple100: Color(0xFFE9D5FF), // Shadow for new items
+      // Neutral Colors (Grays)
+      white: Color(0xFFFFFFFF), // Card backgrounds
+      gray50: Color(0xFFF9FAFB), // General background
+      gray100: Color(0xFFF3F4F6), // Empty state background
+      gray200: Color(0xFFE5E7EB), // Card borders
+      gray300: Color(0xFFD1D5DB), // Input borders, disabled button
+      gray400: Color(0xFF9CA3AF), // Secondary icons
+      gray500: Color(0xFF6B7280), // Secondary text
+      gray600: Color(0xFF4B5563), // Text
+      gray700: Color(0xFF374151), // Titles
+      gray900: Color(0xFF1F2937), // Main text
+      // Success Colors (Green)
+      green500: Color(0xFF10B981), // Success icon
+      green50: Color(0xFFECFDF5), // Success message background
+      green200: Color(0xFFBBF7D0), // Success message border
+      green800: Color(0xFF065F46), // Success message text
+      // Error Colors (Red)
+      red500: Color(0xFFEF4444), // Error icon
+      red50: Color(0xFFFEF2F2), // Error message background
+      red200: Color(0xFFFECACA), // Error message border
+      red800: Color(0xFF991B1B), // Error message text
     ),
   ],
 );
